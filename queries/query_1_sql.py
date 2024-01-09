3aa80a008e16d1d84bff64a62d68aa6c0908f26d
FilipposSevastakis <106911339+FilipposSevastakis@users.noreply.github.com>
2024-01-10 01:42:05 +0200

    Update query_1_sql.py

--- queries/query_1_sql.py
+++ queries/query_1_sql.py
@@ -3,7 +3,9 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import IntegerType, DoubleType, StringType, TimestampNTZType, DateType
 from pyspark.sql.functions import col, to_date, unix_timestamp, year, month
+import time
 
+start = time.time()
 
 spark = SparkSession \
         .builder \
@@ -29,5 +31,7 @@ crime_incidents_df.createOrReplaceTempView("crime_incidents")
 
 query_1 = "SELECT year, month, crime_total, rank FROM (SELECT year(date_occ) AS year, month(date_occ) AS month, count(*) AS crime_total, RANK() OVER (PARTITION BY year(date_occ) ORDER BY count(*) DESC) AS rank FROM crime_incidents GROUP BY year, month) ranked WHERE rank <=3  ORDER BY year, rank"
 
-data = spark.sql(query_1)
-data.show(10)
+spark.sql(query_1).show(45)
+
+end = time.time()
+print("Query 1 SQL - Execution Time: ",(end-start), "s")
