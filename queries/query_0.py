bbd4f90b420c099915e4d50eddfa3db43fb718dd
FilipposSevastakis <106911339+FilipposSevastakis@users.noreply.github.com>
2024-01-11 18:43:17 +0200

    Update query_0.py

--- queries/query_0.py
+++ queries/query_0.py
@@ -7,17 +7,12 @@ spark = SparkSession \
         .appName("DataFrame") \
         .getOrCreate()
 
-crime_incidents_2010_to_2019_df = spark.read.format('csv') \
+crime_incidents_df = spark.read.format('csv') \
         .options(header = True, inferSchema = True) \
-        .load("hdfs://okeanos-master:54310/data/crime_incidents_2010-2019.csv")
-
-crime_incidents_2020_to_curr_df = spark.read.format('csv') \
-        .options(header = True, inferSchema = True) \
-        .load("hdfs://okeanos-master:54310/data/crime_incidents_2020-.csv")
+        .load("hdfs://okeanos-master:54310/data/crime_incidents.csv")
 
 
-crime_incidents_df = crime_incidents_2010_to_2019_df \
-        .union(crime_incidents_2020_to_curr_df) \
+crime_incidents_df = crime_incidents_df \
         .select(
             to_date(unix_timestamp(col("Date Rptd"),"MM/dd/yyyy hh:mm:ss a").cast("timestamp"),"yyyy-MM-dd").alias("Date Rptd"),
             to_date(unix_timestamp(col("DATE OCC"),"MM/dd/yyyy hh:mm:ss a").cast("timestamp"),"yyyy-MM-dd").alias("DATE OCC"),
