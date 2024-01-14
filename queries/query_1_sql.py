2d08690facad9d1348de6264eea5b60457fac252
FilipposSevastakis <106911339+FilipposSevastakis@users.noreply.github.com>
2024-01-14 16:53:17 +0200

    Update query_1_sql.py

--- queries/query_1_sql.py
+++ queries/query_1_sql.py
@@ -12,11 +12,16 @@ spark = SparkSession \
         .appName("Query 1 SQL") \
         .getOrCreate()
 
-crime_incidents_df = spark.read.format('csv') \
+crime_incidents_2010_to_2019_df = spark.read.format('csv') \
         .options(header = True, inferSchema = True) \
-        .load("hdfs://okeanos-master:54310/data/crime_incidents.csv")
+        .load("hdfs://okeanos-master:54310/data/crime_incidents_2010-2019.csv")
 
-crime_incidents_df = crime_incidents_df \
+crime_incidents_2020_to_curr_df = spark.read.format('csv') \
+        .options(header = True, inferSchema = True) \
+        .load("hdfs://okeanos-master:54310/data/crime_incidents_2020-.csv")
+
+crime_incidents_df = crime_incidents_2010_to_2019_df \
+        .union(crime_incidents_2020_to_curr_df) \
         .select(
             to_date(unix_timestamp(col("DATE OCC"),"MM/dd/yyyy hh:mm:ss a").cast("timestamp"),"yyyy-MM-dd").alias("date_occ"),
             )
