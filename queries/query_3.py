cd9450b23a772b38a62515ad187ac37f7c2e9fe4
FilipposSevastakis <106911339+FilipposSevastakis@users.noreply.github.com>
2024-01-15 14:59:08 +0200

    Update query_3.py

--- queries/query_3.py
+++ queries/query_3.py
@@ -3,6 +3,9 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import IntegerType, DoubleType, StringType, TimestampNTZType, DateType
 from pyspark.sql.functions import col, split, regexp_replace, desc, year, to_date, unix_timestamp, when
+import time
+
+start = time.time()
 
 spark = SparkSession \
         .builder \
@@ -12,6 +15,13 @@ spark = SparkSession \
 crime_incidents_2010_to_2019_df = spark.read.format('csv') \
         .options(header = True, inferSchema = True) \
         .load("hdfs://okeanos-master:54310/data/crime_incidents_2010-2019.csv")
+
+crime_incidents_2020_to_curr_df = spark.read.format('csv') \
+        .options(header = True, inferSchema = True) \
+        .load("hdfs://okeanos-master:54310/data/crime_incidents_2020-.csv")
+
+crime_incidents_df = crime_incidents_2010_to_2019_df \
+        .union(crime_incidents_2020_to_curr_df) \
 
 LA_income_2015_df = spark.read.format('csv') \
         .options(header = True, inferSchema = True) \
@@ -23,9 +33,10 @@ rev_geocoding_df = spark.read.format('csv') \
 
 rev_geocoding_unique_df = rev_geocoding_df.withColumn("ZIPcode", split(col("ZIPcode"), ";").getItem(0))\
 
-crime_incidents_2015_df = crime_incidents_2010_to_2019_df.select(col("Vict Descent"), col("LAT"), col("LON"), col("DATE OCC")) \
+crime_incidents_2015_df = crime_incidents_df.select(col("Vict Descent"), col("LAT"), col("LON"), col("DATE OCC")) \
         .withColumn("year", year(to_date(unix_timestamp(col("DATE OCC"), "MM/dd/yyyy hh:mm:ss a").cast("timestamp"),"yyyy-MM-dd"))) \
         .filter((col("year") == 2015) & (col("Vict Descent").isNotNull()))
+
 
 crime_incidents_2015_zip_df = crime_incidents_2015_df.join(rev_geocoding_unique_df, ['LAT', 'LON']) \
         .select(col("Vict Descent"), col("ZIPcode"))
@@ -47,18 +58,33 @@ best_worst_3_df = distinct_LA_incomes_df.orderBy(col("Estimated Median Income"))
 
 query_3_df = crime_incidents_2015_zip_df.join(best_worst_3_df, "ZIPcode") \
         .withColumn(
-                "Victim Descent", 
-                when((col("Vict Descent") == "A") | (col("Vict Descent") == "C") | (col("Vict Descent") == "K") | (col("Vict Descent") == "J") | (col("Vict Descent") == "F"), "Asian") \
-                .when((col("Vict Descent") == "B"), "Black") \
-                .when((col("Vict Descent") == "W"), "White") \
-                .when((col("Vict Descent") == "O") | (col("Vict Descent") == "I"), "Other") \
-                .when((col("Vict Descent") == "H"), "Hispanic/Latin/Mexican") \
-                .when((col("Vict Descent") == "X"), "Unknown")
-                ) \
-        .groupBy(col("Victim Descent")).count() \
+                    "Victim Descent",
+                    when(col("Vict Descent") == "A", "Other Asian") \
+                    .when(col("Vict Descent") == "B", "Black") \
+                    .when(col("Vict Descent") == "C", "Chinese") \
+                    .when(col("Vict Descent") == "D", "Cambodian") \
+                    .when(col("Vict Descent") == "F", "Filipino") \
+                    .when(col("Vict Descent") == "G", "Guamanian") \
+                    .when(col("Vict Descent") == "H", "Hispanic/Latin/Mexican") \
+                    .when(col("Vict Descent") == "I", "American Indian/Alaskan Native") \
+                    .when(col("Vict Descent") == "J", "Japanese") \
+                    .when(col("Vict Descent") == "K", "Korean") \
+                    .when(col("Vict Descent") == "L", "Laotian") \
+                    .when(col("Vict Descent") == "P", "Pacific Islander") \
+                    .when(col("Vict Descent") == "S", "Samoan") \
+                    .when(col("Vict Descent") == "U", "Hawaiian") \
+                    .when(col("Vict Descent") == "V", "Vietnamese") \
+                    .when(col("Vict Descent") == "W", "White") \
+                    .when(col("Vict Descent") == "X", "Unknown") \
+                    .when(col("Vict Descent") == "Z", "Asian Indian") \
+                    .otherwise("Unknown")
+                    ) \
+        .groupBy(col("ZIPcode")).count() \
         .orderBy(col("count").desc()) \
-        .select(col("Victim Descent"), col("count").alias("#"))
+        .select(col("ZIPcode"), col("count").alias("#"))
 
 
 query_3_df.show()
 
+end = time.time()
+print("Query 3 - Execution Time: ",(end-start), "s")
