82f6685944d0cb67822dcd5275b0d9057ad16d69
FilipposSevastakis <106911339+FilipposSevastakis@users.noreply.github.com>
2024-01-16 01:49:47 +0200

    Update query_4_closest.py

--- queries/query_4_closest.py
+++ queries/query_4_closest.py
@@ -3,7 +3,7 @@
 from pyspark.sql.window import Window
 from pyspark.sql import SparkSession
 from pyspark.sql.types import IntegerType, DoubleType, StringType, TimestampNTZType, DateType
-from pyspark.sql.functions import col, regexp_replace, desc, year, to_date, unix_timestamp, when, udf, count, avg, format_number, row_number
+from pyspark.sql.functions import col, regexp_replace, desc, year, to_date, unix_timestamp, when, udf, count, avg, format_number, row_number, monotonically_increasing_id
 from geopy.distance import geodesic
 
 def get_distance(lat1, long1, lat2, long2):
@@ -39,32 +39,30 @@ crime_incidents_df = crime_incidents_2010_to_2019_df \
         .union(crime_incidents_2020_to_curr_df) \
         .filter((col("Weapon Used Cd").startswith("1")) & (col("LAT") != "0")) \
         .select(
-            col("DR_NO"),
             to_date(unix_timestamp(col("DATE OCC"),"MM/dd/yyyy hh:mm:ss a").cast("timestamp"),"yyyy-MM-dd").alias("DATE OCC"),
             col("LAT").cast(DoubleType()),
             col("LON").cast(DoubleType()),
             col("AREA ").cast(IntegerType()).alias("PREC")
-            )
-
-# duplicates = crime_incidents_df.groupBy(crime_incidents_df.columns).count().filter(col("count") > 1)
-# print(duplicates.count())
+            ) \
+        .withColumn("ID", monotonically_increasing_id())
 
 
-crime_x_stations_df = crime_incidents_df.crossJoin(LAPD_Police_Stations_df) \
+crime_x_stations_df = crime_incidents_df.join(LAPD_Police_Stations_df) \
         .withColumn("year", year("DATE OCC")) \
         .withColumn("distance", get_distance_udf(col("LAT"), col("LON"), col("Y"), col("X")))
 
 
-closest_window = Window.partitionBy("DR_NO", "LAT", "LON", "year").orderBy("distance")
+closest_window = Window.partitionBy("ID").orderBy("distance")
 
 crime_x_station_df = crime_x_stations_df \
-        .withColumn("number_in_part", row_number().over(closest_window)) \
-        .filter(col("number_in_part") == 1) 
+        .withColumn("number_in_partition", row_number().over(closest_window)) \
+        .filter(col("number_in_partition") == 1) \
+        .persist()
 
 
 query_4a_df = crime_x_station_df \
         .groupBy("year").agg(
-                format_number(avg(col("distance")), 4).alias("average_distance"),
+                format_number(avg(col("distance")), 3).alias("average_distance"),
                 count("*").alias("#")
                 ) \
         .orderBy(col("year")) \
@@ -72,9 +70,8 @@ query_4a_df = crime_x_station_df \
 
 query_4b_df = crime_x_station_df \
         .groupBy("division").agg(
-                format_number(avg(col("distance")),4).alias("average_distance"),
+                format_number(avg(col("distance")),3).alias("average_distance"),
                 count("*").alias("#")
                 ) \
         .orderBy(col("#").desc()) \
         .show(25)
-
